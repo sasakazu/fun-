c44747fc96aba0af9a2c185cd1d606c5d3a6455d
sasakazu <dropout.college2014@gmail.com>
2017-07-08 18:43:56 +0900

    base done.....

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+
+
+  private
+
+      def user_params
+        params.require(:user).permit()
+      end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
+
+  resources :users
+
   root 'pages#home'
 
   get 'pages/about'
